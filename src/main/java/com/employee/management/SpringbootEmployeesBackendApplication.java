17051e10b3016ec24c84cb03309abda1d64daeaa
Akhilesh <akhil28021996.18@gmail.com>
2022-11-13 21:16:14 +0530

    second initial commit

---
+++ src/main/java/com/employee/management/SpringbootEmployeesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.employee.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmployeesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEmployeesBackendApplication.class, args);
+	}
+
+}
